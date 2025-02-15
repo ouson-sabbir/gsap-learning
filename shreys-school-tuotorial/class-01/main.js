df11b97f73def846faf28e07f10cb7e1d15bb833
ouson-sabbir <ousonsabbir4@gmail.com>
2025-02-15 14:11:25 +0600

    add some gsap tutorial existing files class 01

---
+++ shreys-school-tuotorial/class-01/main.js
@@ -0,0 +1,18 @@
+gsap.to("#box1",{
+    x: 1200,
+    duration: 2,
+    delay: 1,
+    rotate: 360,
+    backgroundColor: "#12680f",
+    repeat: -1,
+    yoyo: true,
+});
+gsap.from("#box2",{
+    x: 1200,
+    duration: 2,
+    delay:1,
+    rotate: 360,
+    backgroundColor: "#12680f",
+    repeat: -1,
+    yoyo: true,
+})
